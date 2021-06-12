498fea626f13fa3a4013cf615e890597f7197cf2
GILBERT.MARQUEZ <gilbert.marquez@ucr.ac.cr>
2021-06-12 15:06:35 -0600

    ConsumerClient class name changed to HttpConnectionHandler. Added HttpConnectionHandler.hpp

--- src/HttpConnectionHandler.hpp
+++ src/HttpConnectionHandler.hpp
@@ -1,23 +1,21 @@
 // Copyright 2021 Rostipollos. Universidad de Costa Rica. CC BY 4.0
 
-#ifndef CONSUMERCLIENT_HPP
-#define CONSUMERCLIENT_HPP
+#ifndef HTTPCONNECTIONHANDLER_HPP
+#define HTTPCONNECTIONHANDLER_HPP
 
 #include "Consumer.hpp"
 #include "Socket.hpp"
 
-class ConsumerClient : public Consumer<Socket> {
-  DISABLE_COPY(ConsumerClient);
-
-  protected:
-
+class HttpConnectionHandler : public Consumer<Socket> {
+  DISABLE_COPY(HttpConnectionHandler);
+    
   public:
     // Constructor
-    explicit ConsumerTest(int consumerDelay);
+    explicit HttpConnectionHandler();
     // Consume the messages in its own execution thread
     int run() override;
     // Override this method to process any data extracted from the queue
-    void consume(const Socket data) override;
+    void consume(const Socket client) override;
 };
 
-#endif  // CONSUMERCLIENT_HPP
+#endif  // HTTPCONNECTIONHANDLER_HPP
