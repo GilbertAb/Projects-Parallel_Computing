b09f8de0f2178168943b55147b003a7c2e0ed905
David Atias <david.atias@ucr.ac.cr>
2021-07-19 01:29:53 -0600

    Added analysis for distributed execution | Used more readble data in graphs | added openmp flag to release rule in makefile. Close #14

--- proyecto-2/src/main.cpp
+++ proyecto-2/src/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]) {
   if (MPI_Init(&argc, &argv) == MPI_SUCCESS) {
   Job job;
-  int process_count = -1, rank = -1;
+  int process_count = 0, rank = 0;
   MPI_Comm_size(MPI_COMM_WORLD, &process_count);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   return job.run(argc, argv, process_count, rank);
